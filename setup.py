54e9d62439722352490654e684ea1675aa4fa4ac
thakursamir0000-star <thakursamir0000@gmail.com>
2026-04-11 15:34:48 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Build ML project that predicts foof delivery time',
+    author='Samir Thakur',
+    license='',
+)
